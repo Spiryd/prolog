775d682689331ed1cab8db348804c2fc85e518b8
Spiryd <neumann.maks@gmail.com>
2024-03-11 18:51:07 +0100

    Update 3.pl

--- l1/3.pl
+++ l1/3.pl
@@ -1,11 +1,19 @@
-is_prime(2).
-is_prime(N) :- is_prime(N, 2).
-is_prime(N, N).
-is_prime(N, K) :-
-    N mod K =\= 0,
-    K1 is K + 1,
-    is_prime(N, K1).
+is_prime(2) :-
+    !.
+is_prime(3) :-
+    !.
+is_prime(X) :-
+    X > 3,
+    X mod 2 =\= 0,
+    is_prime_(X, 3).
 
+is_prime_(X, N) :-
+    (  N*N > X
+    -> true
+    ;  X mod N =\= 0,
+       M is N + 2,
+       is_prime_(X, M)
+    ).
 prime(LO, HI, N) :-
     between(LO, HI, N),
     is_prime(N).
