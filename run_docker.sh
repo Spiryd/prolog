be3b9add0147c717efbc4d69591f525ff5eae8a6
Spiryd <neumann.maks@gmail.com>
2024-04-22 10:12:15 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,5 @@ docker stop prolog
 docker rm prolog
 docker build . -t prolog --network host
 docker run --name prolog -d -i -t prolog /bin/sh
+docker rmi $(docker images -f dangling=true -q)
 docker exec -it prolog bash
